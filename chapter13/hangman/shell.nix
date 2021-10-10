4ed343cb44b972efdd005344058d7124cd735004
alternateved <alternateved@gmail.com>
2021-10-10 18:04:08 +0200

    Rework nix files

--- chapter13/hangman/shell.nix
+++ chapter13/hangman/shell.nix
@@ -5,15 +5,14 @@ let
   inherit (nixpkgs) pkgs;
   inherit (pkgs) haskellPackages;
 
+  package = import ./. pkgs;
+
   haskellTools = with haskellPackages; [ ghc ghcid cabal-install ];
-
-  haskellDeps = with haskellPackages; [ random split ];
-
-  allPackages = haskellTools ++ haskellDeps;
 
 in pkgs.stdenv.mkDerivation {
   name = "hangmanShell";
-  buildInputs = allPackages;
+  inputsFrom = [ package.env ];
+  buildInputs = haskellTools;
   shellHook = ''
     export PS1="\[hs:\033[1;32m\]\W\[\033[0m\] ~ "
   '';
