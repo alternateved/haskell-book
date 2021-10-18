1531bc7738c4a265c1a466d37c6afbba8f1cfb55
alternateved <alternateved@gmail.com>
2021-10-18 17:36:16 +0200

    Initialize morse project

---
+++ chapter14/morse/shell.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import ./packages.nix { };
+  package = import ./.;
+
+  haskellTools = with pkgs; [
+    ghc
+    ghcid
+    cabal-install
+    haskellPackages.hindent
+    haskellPackages.hoogle
+    haskellPackages.ormolu
+    haskellPackages.haskell-language-server
+  ];
+
+  haskellDeps = with pkgs; [ haskellPackages.split haskellPackages.random ];
+
+in pkgs.mkShell {
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  inputsFrom = [ package.env ];
+  buildInputs = haskellTools ++ haskellDeps;
+  shellHook = ''
+    export PS1="\[hs:\033[1;32m\]\W\[\033[0m\] ~ "
+  '';
+}
---
+++ chapter14/morse/src/Morse.hs
@@ -0,0 +1,5 @@
+module Morse (Morse, charToMorse, stringToMorse, letterToMorse, morseToLetter) where
+
+import qualified Data.Map as M
+
+type Morse = String
